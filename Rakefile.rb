31637f31df7c5874ea3764eef67c123d4c101a2e
Casey West <casey@geeknest.com>
2014-12-30 11:20:00 -0500

    Better rakefile

--- Rakefile.rb
+++ Rakefile.rb
@@ -38,8 +38,6 @@ task :new_post, :title do |t, args|
     post.puts "  feature: "
     post.puts "  credit: "
     post.puts "  creditlink: "
-    post.puts "comments: "
-    post.puts "share: "
     post.puts "---"
   end
 end
@@ -69,7 +67,6 @@ task :new_page, :title do |t, args|
     page.puts "  feature: "
     page.puts "  credit: "
     page.puts "  creditlink: "
-    page.puts "share: "
     page.puts "---"
   end
 end
@@ -86,4 +83,37 @@ def ask(message, valid_options)
     answer = get_stdin(message)
   end
   answer
+end
+
+def git_clean?
+  git_state = `git status 2> /dev/null | tail -n1`
+  clean = (git_state =~ /working directory clean/)
+end
+
+task :check_git do
+  unless git_clean?
+    puts "Dirty repo - commit or discard your changes and run deploy again"
+    exit 1
+  end
+end
+
+desc "Deploy to remote origin"
+task :deploy => [:check_git] do
+  source_branch = 'source'
+  deploy_branch = 'master'
+  message = "Site updated at #{Time.now.utc}"
+
+  system "bundle exec jekyll build"
+  system "git checkout \"#{deploy_branch}\""
+  system "cp -r _site/* . && rm -rf _site/ && touch .nojekyll"
+
+  unless git_clean?
+    system "git add . && git commit -m \"#{message}\""
+    system "git push origin \"#{deploy_branch}\""
+    puts "Pushed to origin with commit message: #{message}"
+  else
+    puts "No changes to deploy - canceled"
+  end
+
+  system "git checkout \"#{source_branch}\""
 end
